7863c897cd890e5e1995b29072143dd5e42e4495
yassine <yassine@nassibi.com>
2022-09-06 12:41:59 +0200

    introduction

---
+++ Introduction/index.php
@@ -0,0 +1,30 @@
+<?php 
+$title ="My First PHP Page" ;
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?PHP echo "UTF-8";?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php  echo $title ;?></title>
+  </head>
+  <body>
+    <div><?php echo "We Love" ;?></div>
+    <div><?php echo "Elzero Channel" ;?></div>
+     <!-- task 2 -->
+     <?php
+    //    echo 'Prevent Me From Running Please';
+     ## echo 'Prevent Me From Running Please';
+      /* echo 'Prevent Me From Running Please'*/;
+     ?>
+     <!-- ________________________________ -->
+     <!-- task 3 -->
+     <?php 
+     /* My Application
+     Version 1.0
+     Created By Elzero */
+     ?>
+      <!-- ___________________________________ -->
+  </body>
+</html>
